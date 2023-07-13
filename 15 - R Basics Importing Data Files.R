8fd48646561fbd83d8ce4819b589b10337b182cd
STSx666 <Jeff.29A.D@gmail.com>
2023-07-13 14:49:55 -0600

    Code for video 15

---
+++ 15 - R Basics Importing Data Files.R
@@ -0,0 +1,8 @@
+mm_df <- read.csv('C:/Users/Belial/Desktop/MM_Madison.csv')
+mm_df
+
+View(mm_df)
+
+mm_df2 <- read.delim('C:/Users/Belial/Desktop/MM_Madison.tsv',
+                     sep = '\t')
+mm_df2
